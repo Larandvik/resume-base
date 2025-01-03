3b35cf2922d8836d3648299cb4d17e5d11ccfb2b
Larandvik <0658203@gmail.com>
2025-01-03 20:07:01 -0300

    13.2 Add ResumeServlet

---
+++ src/main/java/com/larandvik/webapp/web/ResumeServlet.java
@@ -0,0 +1,24 @@
+package com.larandvik.webapp.web;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello Resumes" : "Hello " + name + '!');
+    }
+}
